bdb1cd64b6ba78d3085aee1903c814ff00474722
kdkIshwor <khadkaishwor44@gmail.com>
2022-10-05 20:19:01 +1100

    fixed the liquidity error

--- pages/index.js
+++ pages/index.js
@@ -518,7 +518,7 @@ export default function Home() {
       </div>
 
       <footer className={styles.footer}>
-        Made with &#10084; by Ishwor Khadka
+        Made with &#10084; by Crypto Devs
       </footer>
     </div>
   );
